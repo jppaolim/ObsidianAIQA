5abf148cb0077bee1ad42d1825a2e2b6d2a577af
Jean-Paul Paoli <contact@paolim.fr>
2023-05-24 02:20:55 +0200

    full databse

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# MDFileRepo-QA
+# Obisidian Ai Question Answering 
 how to query a folder full of md files through LLM
+
+### How to use 
+
+1. Link model 
+
+```
+ln -s -v /Users/XXX/Obsidian/VaultName/pathToFolder  ./Ressources 
+```
+
+2. Link directory 
+
+```
+ln -s -v /Users/XXX/Obsidian/VaultName/pathToFolder  ./Ressources 
+```
+
+3. populate env. file 
+
+Based on envtemplate create a .env file
--- qadoc.py
+++ qadoc.py
@@ -19,6 +19,7 @@ from langchain.prompts.chat import (
 from langchain.prompts import PromptTemplate
 
 from langchain.chains import RetrievalQA
+from langchain.chains import LLMChain, HypotheticalDocumentEmbedder
 
 # from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
@@ -32,7 +33,7 @@ DOC_DIRECTORY = os.environ.get("DOC_DIRECTORY")
 ## embeddings parameter  >> instructor model : https://arxiv.org/pdf/2212.09741.pdf
 INSTRUCT_MODEL=os.environ.get("INSTRUCT_MODEL")
 CHUNK_SIZE=int(os.environ.get("CHUNK_SIZE"))
-OVERLAP = 16
+OVERLAP = 120
 PERSIST_DIRECTORY = os.environ.get("PERSIST_DIRECTORY")
 
 ## llm and API key 
@@ -45,7 +46,11 @@ PROMPTLAYER_API_KEY=os.environ.get("PROMPTLAYER_API_KEY")
 PRINT_SOURCE = (os.getenv('PRINT_SOURCE', 'False') == 'True')
 BUILD_REFRESH_DB = (os.getenv('BUILD_REFRESH_DB', 'False') == 'True')
 
-
+query1 =  "Can we build a moat for an AI startup ?"
+query2 =  "Should we be afraid of AI ?"
+query3 =  "Who do you think is going to win in generative AI space ? Name the potential actors or cluster them and comment on the pros and cons."
+query4 =  "Find 3 different ways to explain the difference between AI and generative AI using analogies to a college level student and without technical jargon"
+thequery=query3
 
 # ****************  Embeddings function 
 from langchain.embeddings import HuggingFaceInstructEmbeddings
@@ -68,6 +73,7 @@ from langchain.vectorstores import FAISS
 def create_or_load_db_faiss():
 
     #see if we need to build 
+    faissDir = "./" + PERSIST_DIRECTORY + "/faiss_index/" 
     faissfile1 = "./" + PERSIST_DIRECTORY + "/faiss_index/index.faiss" 
     faissfile2 = "./" + PERSIST_DIRECTORY + "/faiss_index/index.pkl" 
     missingfile = not (os.path.exists(faissfile1) and os.path.exists(faissfile2))
@@ -86,30 +92,24 @@ def create_or_load_db_faiss():
             docdirectory, glob="**/*.md", loader_cls=UnstructuredMarkdownLoader)
 
         documents = loader.load()
-
-        start_time = time.time()
         texts = text_splitter.split_documents(documents)
 
-        elapsed_time = time.time() - start_time
-
+        print(f"Loaded {len(documents)} documents from {DOC_DIRECTORY}")
+        print(f"Split into {len(texts)} chunks of text (max. {CHUNK_SIZE} char each). Chunk distribution is as follow: ")
         textspd = pd.Series(map(lambda x:len(x.page_content), texts))
         stats = textspd.describe()
         print(stats)
 
-        print(f"Loaded {len(documents)} documents from {DOC_DIRECTORY}")
-        print(f"Split into {len(texts)} chunks of text (max. {CHUNK_SIZE} char each)")
-        print(f"it took  {elapsed_time} seconds to process ")
-
         start_time = time.time()
-        
-        db = FAISS.from_documents(texts, embeddings_function())
-        
+        db = FAISS.from_documents(texts, embeddings_function())    
+        #db = FAISS.from from_documents(texts, embeddings_function())    
         elapsed_time = time.time() - start_time
         print(f"it took  {elapsed_time} seconds to create the database with indicated embeddings ")
         
-        db.save_local(faissfile)
+        db.save_local(faissDir)
+
     else:
-        db = FAISS.load_local(faissfile, embeddings_function()) 
+        db = FAISS.load_local(faissDir, embeddings_function()) 
     
     return db 
 
@@ -130,7 +130,7 @@ def customize_chat_prompt():
     
     return PROMPT
 
-def customize_chat_prompt_LLAMA():
+def customize_LLAMA_prompt():
 
     prompt_template = """### Human : You act as an helpful and insightful research assistant. Use you own knowledge and incorporate the most relevant points of the following context to write a nuanced, well argumented and credible anwer to the question. Here is the context : 
 
@@ -174,10 +174,15 @@ def go_and_interact(qa : RetrievalQA):
     return
 
 def go_and_answer(query: str, qa : RetrievalQA):
+    
+    start_time = time.time()
     res = qa(query)
+    elapsed_time = time.time() - start_time
+  
+    print("\n")
+    print(f"it took  {elapsed_time} seconds to put up following answer : ")
     answer  = res['result']  
     print(answer)
-    print("\n")
     
     return
 
@@ -185,40 +190,89 @@ def go_and_answer(query: str, qa : RetrievalQA):
 def main():
 
     db = create_or_load_db_faiss()
-    querybase = db.as_retriever(search_type="mmr", search_kwargs={"k":4, "lambda_mult":0.7})
+
+
 
     if LLMTYPE=="ChatGPT":
+        dbretriever = db.as_retriever(search_type="mmr", search_kwargs={"k":5, "lambda_mult":0.9})
+
         PROMPT=customize_chat_prompt() 
-
         chain_type_kwargs = {"prompt": PROMPT} 
-        qa = RetrievalQA.from_chain_type(llm=PromptLayerChatOpenAI(openai_api_key=OPENAI_KEY, temperature=0.1, max_tokens=2048, verbose=True), chain_type="stuff", retriever=querybase, chain_type_kwargs=chain_type_kwargs)
-
-        qb = RetrievalQA.from_chain_type(llm=PromptLayerChatOpenAI(openai_api_key=OPENAI_KEY, temperature=0.7, max_tokens=2048, verbose=True), chain_type="stuff", retriever=querybase, chain_type_kwargs=chain_type_kwargs)
+        qa = RetrievalQA.from_chain_type(llm=PromptLayerChatOpenAI(openai_api_key=OPENAI_KEY, temperature=0.1, max_tokens=2048, verbose=True), chain_type="stuff", retriever=dbretriever, chain_type_kwargs=chain_type_kwargs)
+        qb = RetrievalQA.from_chain_type(llm=PromptLayerChatOpenAI(openai_api_key=OPENAI_KEY, temperature=0.7, max_tokens=2048, verbose=True), chain_type="stuff", retriever=dbretriever, chain_type_kwargs=chain_type_kwargs)
 
     elif LLMTYPE=="LLAMACPP":
-        llamamodel = LlamaCpp(model_path=MODEL_PATH, n_threads=6,  n_ctx=2048, max_tokens=512, temperature = 0.7, top_k = 50, top_p=0.9, verbose=True)
+        dbretriever = db.as_retriever(search_type="mmr", search_kwargs={"k":5, "lambda_mult":0.9})
 
-        PROMPT=customize_chat_prompt_LLAMA() 
+        llamamodel = LlamaCpp(model_path=MODEL_PATH, n_threads=6,  n_ctx=2048, max_tokens=400,  temperature = 0.8, top_k = 50, top_p=0.9, verbose=True, use_mlock=True)
+        PROMPT=customize_LLAMA_prompt() 
         chain_type_kwargs = {"prompt": PROMPT} 
-        qa = RetrievalQA.from_chain_type(llm=llamamodel, chain_type="stuff", retriever=querybase, chain_type_kwargs=chain_type_kwargs)
+        qa = RetrievalQA.from_chain_type(llm=llamamodel, chain_type="stuff",  retriever=dbretriever, chain_type_kwargs=chain_type_kwargs)
         #qb = RetrievalQA.from_chain_type(llm=llamamodel, chain_type="stuff", retriever=querybase, chain_type_kwargs=chain_type_kwargs)
 
-    else:
-        print("Mistake in LLM")
-        exit
+    elif LLMTYPE=="LLAMACPPV2":
+        dbretriever = db.as_retriever(search_type="mmr", search_kwargs={"k":5, "lambda_mult":0.9})
 
-    #go_and_answer("Should we be afraid of AI ?", qa)
-    #go_and_answer("Should we be afraid of AI ?", qb)
-   
-    #go_and_answer("Please build a scenario of how an AI could take over the world. Brainstorm about mitigation tactics.", qa)
-    #go_and_answer("Please build a scenario of how an AI could take over the world. Brainstorm about mitigation tactics.", qb)
+        prompt_template = """### Human :You act as an insightful assistant. Use you own knowledge but incorporate the most relevant points of the following context to write a nuanced, well argumented and credible article based on the user query. In case you have no firm opinion list pros and cons. In case you have one, defend it well. Make hypothesis to produce a point of view. Avoid falsehood. Add a section about how this point of view could be wrong.
+        Here is the context : 
+        {context}
+        Now is the query: {question}
+        ### Assistant :"""
 
-    go_and_answer("Can we build a moat for an AI startup ?", qa)
+        PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+        context = dbretriever.get_relevant_documents(thequery) 
+        print(f"Context for {thequery} : ")
+        inputcontext = ""
+        for doc in context:
+            print(doc)
+            print("\n")
+            inputcontext = inputcontext + doc.page_content + "\n"   
+        
+        llamamodel = LlamaCpp(model_path=MODEL_PATH, n_threads=6,  n_ctx=2048, max_tokens=400,  temperature = 0.7, top_k = 60, top_p=0.85, verbose=True, use_mlock=True)
+        
+        chain = LLMChain(llm=llamamodel, prompt=PROMPT) 
+ 
+        inputs = [{"context": inputcontext, "question": thequery}]
+        print(chain.apply(inputs)[0]["text"]) 
+
+    elif LLMTYPE=="LLAMACPPV3":
+        dbretriever = db.as_retriever(search_type="mmr", search_kwargs={"k":5, "lambda_mult":0.9})
+
+        llamamodel = LlamaCpp(model_path=MODEL_PATH, n_threads=6,  n_ctx=2048, max_tokens=400,  temperature = 0.7, top_k = 60, top_p=0.85, verbose=True, use_mlock=True)
+
+        prompt_template = """### Human : You act as an helpful and insightful research assistant. Please write a nuanced, well argumented and credible anwer to the user question. 
+        {question}
+        ### Assistant :"""
+        prt = PromptTemplate(input_variables=["question"], template=prompt_template)
+        hydeChain = LLMChain(llm=llamamodel, prompt=prt)
+
+        hydeEmbeddings = HypotheticalDocumentEmbedder(llm_chain=hydeChain, base_embeddings=embeddings_function())
     
+        result = hydeEmbeddings.embed_query(thequery)
+        context =  db.max_marginal_relevance_search_by_vector(result, k=5, lambda_mult=0.9)
+        
+        print(f"Context for {thequery} : ")
+        inputcontext = ""
+        for doc in context:
+            print(doc)
+            print("\n")
+            inputcontext = inputcontext + doc.page_content + "\n"   
+        
+        PROMPT=customize_LLAMA_prompt() 
+        chain = LLMChain(llm=llamamodel, prompt=PROMPT) 
    
+        inputs = [{"context": inputcontext, "question": query1}]
+        print(chain.apply(inputs)[0]["text"]) 
+        
+        
+    else:
+        raise SystemExit("Mistake in LLM")
+
+    #go_and_answer(query1, qa)
+    
     db = None
     exit
-
 
 if __name__ == "__main__":
     main()

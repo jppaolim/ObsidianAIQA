9185c4df4e55d3b6ecdefe89dcbf2ac40406f21f
Jean-Paul Paoli <contact@paolim.fr>
2023-05-31 23:12:55 +0200

    refactor, more clear code

--- qadoc.py
+++ qadoc.py
@@ -1,10 +1,8 @@
 import os
-import re
-import requests
-import time
+#import re
+#import requests
 
-
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 from langchain.llms import OpenAI, LlamaCpp
 
@@ -86,92 +84,68 @@ def go_and_interact(qa : RetrievalQA):
 
     return
 
-def go_and_answer(query: str, qa : RetrievalQA):
-    
-    start_time = time.time()
-    res = qa(query)
-    elapsed_time = time.time() - start_time
-  
-    print("\n")
-    print(f"it took  {elapsed_time} seconds to put up following answer : ")
-    answer  = res['result']  
-    print(answer)
-    
-    return
-
-
 
 def main():
 
     db = create_or_load_db_faiss()
+    PROMPT=customize_prompt(PROMPTFILE) 
+    chain_type_kwargs = {"prompt": PROMPT} 
+
 
     if LLMTYPE=="ChatGPT":
         
         dbretriever = db.as_retriever(search_type="mmr", search_kwargs={"k":5, "lambda_mult":0.9})
-        PLChat = PromptLayerChatOpenAI(openai_api_key=OPENAI_KEY, temperature=0.1, max_tokens=2048, verbose=True)
+        PLChat = PromptLayerChatOpenAI(openai_api_key=OPENAI_KEY, temperature=0.1, max_tokens=2048, verbose=True, streaming=True, callbacks=[StreamingStdOutCallbackHandler()])
 
-        PROMPT=customize_prompt(PROMPTFILE) 
-        chain_type_kwargs = {"prompt": PROMPT} 
+        inputsdict = {"query": thequery}
         chain = RetrievalQA.from_chain_type(llm=PLChat, chain_type="stuff", retriever=dbretriever, chain_type_kwargs=chain_type_kwargs)
-        res = chain(thequery)
-        answer  = res['result']  
-        print(answer)
-        
-    elif LLMTYPE=="LLAMACPP-AUTOQA":
 
-        dbretriever = db.as_retriever(search_type="mmr", search_kwargs={"k":5, "lambda_mult":0.9})
-        llamamodel = LlamaCpp(model_path=MODEL_PATH, n_threads=6,  n_ctx=2048, max_tokens=400,  temperature = 0.8, top_k = 50, top_p=0.9, verbose=True, use_mlock=True)
-        
-        PROMPT=customize_prompt(PROMPTFILE) 
-        chain_type_kwargs = {"prompt": PROMPT} 
-        chain = RetrievalQA.from_chain_type(llm=llamamodel, chain_type="stuff",  retriever=dbretriever, chain_type_kwargs=chain_type_kwargs)
                 
-    elif LLMTYPE=="LLAMACPP-MANUAL":
+    elif LLMTYPE=="LLAMACPP": 
 
         dbretriever = db.as_retriever(search_type="mmr", search_kwargs={"k":4, "lambda_mult":0.9})
-        llamamodel = LlamaCpp(model_path=MODEL_PATH, n_threads=6,  n_ctx=2048, max_tokens=500,  temperature = 0.7, top_k = 50, top_p=0.75, last_n_tokens_size=256,  n_batch=1024, echo = True, repeat_penalty=1.17647, use_mmap=True, verbose=True, use_mlock=True,  callbacks=Streamcallbacks)
-
-        context = dbretriever.get_relevant_documents(thequery) 
-        print(f"Context for {thequery} : ")
-        inputcontext=build_string_context(context)
-        
-        PROMPT=customize_prompt(PROMPTFILE) 
-        chain = LLMChain(llm=llamamodel, prompt=PROMPT) 
-        inputs = [{"context": inputcontext, "question": thequery}]
- 
-        print(chain.apply(inputs)[0]["text"]) 
-  
-    elif LLMTYPE=="LLAMACPP-HYDE":
-        
-        dbretriever = db.as_retriever(search_type="mmr", search_kwargs={"k":5, "lambda_mult":0.9})
-        llamamodel = LlamaCpp(model_path=MODEL_PATH, n_threads=6,  n_ctx=2048, max_tokens=400,  temperature = 0.7, top_k = 60, top_p=0.85, verbose=True, use_mlock=True)
-
-        prompt_template = """### Human : You act as an helpful and insightful research assistant. Please write a nuanced, well argumented and credible anwer to the user question. 
-        {question}
-        ### Assistant :"""
-        prt = PromptTemplate(input_variables=["question"], template=prompt_template)
-        hydeChain = LLMChain(llm=llamamodel, prompt=prt)
-        hydeEmbeddings = HypotheticalDocumentEmbedder(llm_chain=hydeChain, base_embeddings=embeddings_function())
-    
-        result = hydeEmbeddings.embed_query(thequery)
-        context =  db.max_marginal_relevance_search_by_vector(result, k=5, lambda_mult=0.9)
-        
-        print(f"Context for {thequery} : ")
-        inputcontext = ""
-        for doc in context:
-            print(doc)
-            print("\n")
-            inputcontext = inputcontext + doc.page_content + "\n"   
-        
-        PROMPT=customize_prompt(PROMPTFILE) 
-        chain = LLMChain(llm=llamamodel, prompt=PROMPT) 
+        llamamodel = LlamaCpp(model_path=MODEL_PATH, n_threads=6,  n_ctx=2048, max_tokens=1000,  temperature = 0.7, top_k = 50, top_p=0.75, last_n_tokens_size=256,  n_batch=1024, echo = True, repeat_penalty=1.17647, use_mmap=True, verbose=True, use_mlock=True,  callbacks=Streamcallbacks)
    
-        inputs = [{"context": inputcontext, "question": query1}]
-        print(chain.apply(inputs)[0]["text"]) 
+        if QATYPE =="AUTO":
+
+            inputsdict = {"query": thequery}
+            chain = RetrievalQA.from_chain_type(llm=llamamodel, chain_type="stuff",  retriever=dbretriever, chain_type_kwargs = {"prompt": PROMPT} )
+
+        elif QATYPE=="MANUAL":
+      
+            context = dbretriever.get_relevant_documents(thequery)       
+            print(f"Context for {thequery} : ")
+            inputcontext=build_string_context(context)
+
+            inputsdict = {"context": inputcontext, "question": thequery}
+            chain = LLMChain(llm=llamamodel, prompt=PROMPT) 
+
+        elif QATYPE=="HYDE":
+
+            print("HYDE vanilla generation :")
+            with open(PROMPTFILEHYDE, 'r') as file:
+                prt = file.read()
+            PRT = PromptTemplate(template=prt, input_variables=["question"])
+            hydeChain = LLMChain(llm=llamamodel, prompt=PRT)
+            hydeEmbeddings = HypotheticalDocumentEmbedder(llm_chain=hydeChain, base_embeddings=embeddings_function())     
+            result = hydeEmbeddings.embed_query(thequery)
+
+
+            context =  db.max_marginal_relevance_search_by_vector(result, k=4, lambda_mult=0.9)
+            print(f"Context for {thequery} : ")
+            inputcontext=build_string_context(context)
+            
+            inputsdict = {"context": inputcontext, "question": thequery}
+            chain = LLMChain(llm=llamamodel, prompt=PROMPT) 
         
+        else:
+            raise SystemExit("No corresponding QATYPE")
         
     else:
         raise SystemExit("Mistake in LLM")
+
+
+    res= chain(inputsdict,return_only_outputs=True)
 
     #go_and_answer(query1, qa)
     
